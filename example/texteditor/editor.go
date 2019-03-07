7bc9960fbbd13b5fec09833b740a6efff5f1a5cf
Chunni Deng <chunni.deng@gmail.com>
2019-03-07 05:26:21 -0800

    Basic menu functionality works on ubuntu

--- example/texteditor/editor.go
+++ example/texteditor/editor.go
@@ -1,18 +1,43 @@
 package main
 
-import "github.com/fipress/GoUI"
+import (
+	"github.com/fipress/GoUI"
+	"runtime"
+)
 
 func main() {
-	//goui.Service("chat/:msg", chatService)
-	menuDefs := []goui.MenuDef{
-		{Title: "AppMenu", Type: goui.Container, Children: []goui.MenuDef{
+	var menuDefs []goui.MenuDef
+	switch runtime.GOOS {
+	case "darwin":
+		menuDefs = []goui.MenuDef{{Title: "AppMenu", Type: goui.Container, Children: []goui.MenuDef{
 			{Title: "About", Type: goui.Standard, Action: "about"},
 			{Type: goui.Separator},
 			{Title: "Hide", Type: goui.Standard, Action: "hide"},
 			{Type: goui.Separator},
 			{Title: "Quit", Type: goui.Standard, Action: "quit"},
 		}},
-		{Title: "File", Type: goui.Container, Children: []goui.MenuDef{
+			{Title: "File", Type: goui.Container, Children: []goui.MenuDef{
+				{Title: "New", Type: goui.Custom, Action: "new", Handler: func() {
+					println("new file")
+				}},
+				{Title: "Open", Type: goui.Custom, Action: "open"},
+				{Type: goui.Separator},
+				{Title: "Save", Type: goui.Custom, Action: "save"},
+				{Title: "Save as", Type: goui.Custom, Action: "saveAs"},
+			}},
+			{Title: "Edit", Type: goui.Container, Children: []goui.MenuDef{
+				{Title: "Undo", Type: goui.Custom, Action: "undo"},
+				{Title: "Redo", Type: goui.Custom, Action: "redo"},
+				{Type: goui.Separator},
+				{Title: "Cut", Type: goui.Custom, Action: "cut"},
+				{Title: "Copy", Type: goui.Custom, Action: "copy"},
+				{Title: "Paste", Type: goui.Custom, Action: "paste"},
+			}},
+		}
+	case "windows":
+		println("windows")
+	default:
+		menuDefs = []goui.MenuDef{{Title: "File", Type: goui.Container, Children: []goui.MenuDef{
 			{Title: "New", Type: goui.Custom, Action: "new", Handler: func() {
 				println("new file")
 			}},
@@ -20,15 +45,18 @@ func main() {
 			{Type: goui.Separator},
 			{Title: "Save", Type: goui.Custom, Action: "save"},
 			{Title: "Save as", Type: goui.Custom, Action: "saveAs"},
-		}},
-		{Title: "Edit", Type: goui.Container, Children: []goui.MenuDef{
-			{Title: "Undo", Type: goui.Custom, Action: "undo"},
-			{Title: "Redo", Type: goui.Custom, Action: "redo"},
 			{Type: goui.Separator},
-			{Title: "Cut", Type: goui.Custom, Action: "cut"},
-			{Title: "Copy", Type: goui.Custom, Action: "copy"},
-			{Title: "Paste", Type: goui.Custom, Action: "paste"},
+			{Title: "Quit", Type: goui.Standard, Action: "quit"},
 		}},
+			{Title: "Edit", Type: goui.Container, Children: []goui.MenuDef{
+				{Title: "Undo", Type: goui.Custom, Action: "undo"},
+				{Title: "Redo", Type: goui.Custom, Action: "redo"},
+				{Type: goui.Separator},
+				{Title: "Cut", Type: goui.Custom, Action: "cut"},
+				{Title: "Copy", Type: goui.Custom, Action: "copy"},
+				{Title: "Paste", Type: goui.Custom, Action: "paste"},
+			}},
+		}
 	}
 
 	goui.CreateWithMenu(goui.Settings{
@@ -36,8 +64,9 @@ func main() {
 		Url:    "./ui/editor.html",
 		Top:    30,
 		Left:   100,
-		Width:  300,
-		Height: 440,
+		Width:  430,
+		Height: 450,
+		Debug:  true,
 	},
 		menuDefs)
 }
