b5e936d382ea8e95c6bad99049ab7a91db56811c
Chunni Deng <chunni.deng@gmail.com>
2020-04-07 22:28:21 +0800

    Update filepicker front end to handle attributes

--- widgets/filepicker/js/g-filepicker.js
+++ widgets/filepicker/js/g-filepicker.js
@@ -3,50 +3,71 @@ class GoUIFilePicker extends HTMLElement {
         return ['accept','multiple', 'editable','style','class'];
     }
 
+    bindAttributes() {
+        this.attributesMap = new Map();
+        this.attributesMap.set('action',{dataType:"string"});
+        this.attributesMap.set('message',{dataType:"string"});
+        this.attributesMap.set('accept',{dataType:"string"});
+        this.attributesMap.set('multiple',{dataType: "bool"});
+        this.attributesMap.set('dirOnly',{dataType: "bool"});
+        this.attributesMap.set('fileOnly',{dataType: "bool"});
+        this.attributesMap.set('editable',{el:this.$editor, set:function (v) {
+                const readonly = !v;
+                this.editor.setAttribute("readonly",readonly.toString());
+            }});
+        this.attributesMap.set('class',{el:this.$wrapper,set:function(v) {
+                this.$wrapper.addClass(v);
+            } });
+
+
+        let _this = this;
+        this.attributesMap.forEach(function(v,k) {
+            Object.defineProperty(_this,k,v);
+            if(v.dataType) {
+                v.set = function (val) {
+                    switch (v.dataType) {
+                        case "bool":
+                            _this.data[k] = !!val;
+                            break;
+                        default :
+                            _this.data[k] = val;
+                    }
+                }
+            }
+
+        });
+    }
+
+
     constructor() {
         super();
 
-        this.settings = {
-            //isSave: false,
-            //message: "",
-            //fileTypes: "",
-            //startLocation:"",
-            //suggestedFilename:"",
-            //multiple:false,
-            //fileOnly:false,
-            //dirOnly:false,
-            //allowsOtherFileTypes:false,
-            //canCreateDir:false,
-            //showsHiddenFiles:false,
-        };
-    }
+        this.data = {};
 
-    get editable() {
-        return !this.editor.getAttribute("readonly");
-    }
+        const shadow = this.attachShadow({mode: 'open'});
+        this.$wrapper = $('<span>');
 
-    set editable(v) {
-        const isEditable = Boolean(v);
-        this.editor.setAttribute("readonly",!isEditable);
-    }
+        this.$editor = $('<input type="text">');
+        this.$wrapper.append(this.$editor);
 
-    set accept(v) {
-        //this.fileBtn.setAttribute("accept",v);
-        this.settings.accept = v;
-    }
+        this.$btn = $('<button>Browse...</button>');
+        this.$wrapper.append(this.$btn);
+        shadow.appendChild(this.$wrapper[0]);
 
-    set multiple(v) {
-        const isMulti = Boolean(v);
-        //this.fileBtn.setAttribute("multiple",v);
-        this.settings.multiple = isMulti;
-    }
+        this.bindAttributes();
 
-    set action(v) {
-        this.settings.isSave = v == "save";
+        let _this=this;
+
+        this.$btn.click(function() {
+            goui.request({url:"filepicker",
+                data:_this.data,
+                success:function(path) {
+                    _this.$editor.val(path);
+                }});
+        });
     }
 
     get value() {
-        //return this.fileBtn.value;
         return this.editor.value;
     }
 
@@ -60,88 +81,31 @@ class GoUIFilePicker extends HTMLElement {
     }
 
     connectedCallback() {
-        const el = document.createElement('div');
-        const shadow = el.attachShadow({mode: 'open'});
-        const wrapper = document.createElement('span');
-
-        this.editor = document.createElement('input');
-        this.editor.setAttribute('type', 'text');
-        wrapper.appendChild(this.editor);
-
-        this.btn = document.createElement('button');
-        this.btn.innerText = 'Browse...';
-        wrapper.appendChild(this.btn);
-
-        //this.fileBtn = document.createElement('input');
-        //this.fileBtn.setAttribute('type', 'file');
-        //this.fileBtn.setAttribute('style','opacity:0');
-        //wrapper.appendChild(this.fileBtn);
-
-        // Create some CSS to apply to the shadow dom
-        const style = document.createElement('style');
-
-        style.textContent = `
-      span {
-      	display:grid;
-      	grid-template-columns: auto min-content;
-      	grid-gap: 0; 
-      }
-    `;
-
-        // Attach the created elements to the shadow dom
-        shadow.appendChild(style);
-        //console.log(style.isConnected);
-        shadow.appendChild(wrapper);
-        this.appendChild(el);
-
-        let _this=this;
-
-        this.btn.onclick = function() {
-            //console.log("request goui service");
-            goui.request({url:"filepicker",
-                data:_this.settings,
-                success:function(path) {
-                    _this.editor.value = path;
-                }});
-        };
-
-
-        if(this.hasAttribute("style")) {
-            el.setAttribute("style",this.getAttribute("style"));
+        if(this.attributesMap) {
+            let _this = this;
+            this.attributesMap.forEach(function (v,k) {
+                if(_this.hasAttribute(k)) {
+                    if(v.set) {
+                        v.set(_this.getAttribute(k));
+                    }
+                }
+            });
         }
-
-        if(this.hasAttribute("class")) {
-            //const s = getComputedStyle(this);
-            el.setAttribute("class",this.getAttribute("class"));
-        }
-
-        if(this.hasAttribute("multiple")) {
-            //_this.fileBtn.setAttribute("multiple",this.getAttribute("multiple"));
-            const isMulti = Boolean(this.getAttribute("multiple"));
-            //this.fileBtn.setAttribute("multiple",v);
-            _this.settings.multiple = isMulti;
-        }
-
-        if(this.hasAttribute("accept")) {
-            //_this.fileBtn.setAttribute("accept",this.getAttribute("accept"));
-            _this.settings.accept = this.getAttribute("accept");
-        }
-
-        if(this.hasAttribute("action")) {
-            _this.settings.isSave = this.getAttribute("action") == "save";
-        }
-
-        const readonly = !this.getAttribute("editable");
-        this.editor.setAttribute("readonly",readonly);
-
     }
 
     disconnectedCallback() {
     }
 
     attributeChangedCallback(name, oldValue, newValue) {
-        switch(name) {
-
+        if(this.attributesMap) {
+            let v = this.attributesMap.get(name);
+            if(v && v.set) {
+                var obj = this;
+                if(v.el) {
+                    obj = v.el;
+                }
+                v.set.call(obj,newVal);
+            }
         }
     }
 
